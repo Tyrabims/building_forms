d2b4941257c1ad8ac64db7a13da9463ea276e414
Tyrabims <abimbola.anibaba@yahoo.com>
2023-05-16 14:21:26 +0100

    new

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,31 +1,28 @@
-import React, { FormEvent, useRef } from 'react'
+import React, { FormEvent, useState } from 'react'
+import { useForm } from "react-hook-form";
 
 export const Form = () => {
- const nameRef = useRef<HTMLInputElement>(null);
- const ageRef = useRef<HTMLInputElement>(null);
- const person = { name: '', age: 0 };
-
-  const handleSubmit = (event: FormEvent) => {
-    event.preventDefault();
-    if(nameRef.current !== null){
-      person.name = nameRef.current.value;
-    }
-      
-    if(ageRef.current !== null){
-      person.age = parseInt(ageRef.current.value);
-    }
-      console.log(person)
-  }
+  const { register, handleSubmit, formState:{} } = useForm();
+  const onSubmit = (data: FieldValues) => console.log(data);
 
   return (
     <form onSubmit={handleSubmit}>
       <div className="mb-3">
-        <label htmlFor="name" className="form-label">Name</label>
-        <input ref={nameRef} id="name" type="text" className="form-control" />
+        <label htmlFor="name" className="form-label">
+          Name
+        </label>
+        <input 
+          {...register('name', { required: true, minLength: 3 })}
+            id="name" 
+            type="text" 
+            className="form-control" />
+            { <p>The name field is required.</p>}
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">Age</label>
-        <input ref={ageRef} id="age" type="number" className="form-control" />
+        <input value={person.age}
+          {...register} 
+            id="age" type="number" className="form-control" />
       </div>
       <button className="btn btn-primary" type="submit">Submit</button>
     </form>
